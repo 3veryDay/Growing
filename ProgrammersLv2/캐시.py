a074a837772290f9840ff4cf50375c75bb45432b
지현서 <113419425+3veryDay@users.noreply.github.com>
2025-01-28 11:12:48 +0900

    solve(캐시) : 프로그래머스 Lv 2문제 풀이_ 2차

--- ProgrammersLv2/캐시.py
+++ ProgrammersLv2/캐시.py
@@ -21,3 +21,34 @@ def solution(cacheSize, cities):
             cache.append(city)
             cache.popleft()
     return time
+
+#2차 풀이, 90점
+from collections import deque
+
+def solution(cacheSize, cities) :
+    
+    cache = deque()
+    cache.append(cities[0].lower())
+    idx = 1
+    time = 5
+    
+    while idx < len(cities) :
+        city = cities[idx].lower()
+        if city in cache :
+            time += 1
+            cache.remove(city)
+            cache.append(city)
+            idx += 1
+            
+        if city not in cache :
+            if len(cache) < cacheSize :
+                time += 5
+                cache.append(city)
+                idx += 1
+            else :
+                time += 5
+                cache.append(city)
+                cache.popleft()
+                idx += 1
+            
+    return time
