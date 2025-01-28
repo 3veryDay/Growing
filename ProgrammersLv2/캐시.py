66b223981ac42e382e98bf8f97fe12be70f22091
지현서 <113419425+3veryDay@users.noreply.github.com>
2025-01-28 11:19:12 +0900

    solve(캐시) : 프로그래머스 Lv 2문제 풀이_ 3차

--- ProgrammersLv2/캐시.py
+++ ProgrammersLv2/캐시.py
@@ -52,3 +52,44 @@ def solution(cacheSize, cities) :
                 idx += 1
             
     return time
+
+# 3차 시도 , 100
+
+"""
+cacheSize 가 0부터 ~~ 일 때는 0인 경우도 무조건 확인 같이 해야함.
+보통 return 문으로 일찍 처리하면 될 것.
+
+여기서도 두개의 testcase만 계속 실패가 떴는데, 이는 cacheSize가 0인 경우에도 cache에 넣고 시작했기 때문에 문제 발생.
+0인 경우도 꼭 생각하자. 
+
+"""
+from collections import deque
+
+def solution(cacheSize, cities) :
+    if cacheSize == 0 :
+        return len(cities) * 5
+    cache = deque()
+    cache.append(cities[0].lower())
+    idx = 1
+    time = 5
+    
+    while idx < len(cities) :
+        city = cities[idx].lower()
+        if city in cache :
+            time += 1
+            cache.remove(city)
+            cache.append(city)
+            idx += 1
+            
+        if city not in cache :
+            if len(cache) < cacheSize :
+                time += 5
+                cache.append(city)
+                idx += 1
+            else :
+                time += 5
+                cache.append(city)
+                cache.popleft()
+                idx += 1
+            
+    return time
