d93a151e128d10c788903814cacabd1b07643311
지현서 <113419425+3veryDay@users.noreply.github.com>
2025-02-02 10:57:26 +0900

    롤케이크 자르기에 딕셔너리 메소드 정보 추가

--- ProgrammersLv2/롤케이크 자르기.py
+++ ProgrammersLv2/롤케이크 자르기.py
@@ -19,3 +19,18 @@ def solution(topping) :
     return cnt
     
     
+'''
+1. key pop
+dic.pop('key')
+del dic[key]
+
+dic.popitem() : 딕셔너리의 마지막 항목을 제거하고, 그 항목을 튜플로 반
+
+두개 이상의 딕셔너리를 병합할 때 update() 메소드 사용 가능
+dict 1, dict2가 있을 때. 
+dict1.update(dict2)
+
+merged = dict1 | dict2  3.9 이상 사용 가능 
+
+
+'''
