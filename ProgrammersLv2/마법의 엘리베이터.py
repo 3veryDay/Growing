fdf8b7d87eddb13de6dd41c2dad07b5f98ec67d3
지현서 <113419425+3veryDay@users.noreply.github.com>
2025-02-18 10:52:28 +0900

    Create 마법의 엘리베이터.py

---
+++ ProgrammersLv2/마법의 엘리베이터.py
@@ -0,0 +1,36 @@
+def solution(storey) :
+    
+    cnt = 0
+    s = storey
+    if s < 5 :
+        return s
+    
+    
+    while s != 0 :
+        print(s, cnt)
+        if str(s)[-1] != '0' :
+            one = int(str(s)[-1])
+            two = -1
+            if s > 9 : two = int(str(s)[-2])
+            if one < 5  :
+                s -= one
+                cnt += one
+                
+            elif one > 5 :
+                s += (10-one)
+                cnt += (10 - one)
+            else :  # one이 5일때
+                if two >= 5 :
+                    s += (10-one)
+                    cnt += (10 - one)
+                else :
+                    s -= one
+                    cnt += one
+                
+        s //= 10
+    return cnt
+        
+            
+            
+        
+        
