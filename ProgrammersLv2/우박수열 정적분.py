202404c2609e6781180515be299c272fc44c3d3d
지현서 <113419425+3veryDay@users.noreply.github.com>
2025-03-03 19:42:31 +0900

    점점 성장하고 있어. 15분! solve(우박수열 정적분)

---
+++ ProgrammersLv2/우박수열 정적분.py
@@ -0,0 +1,24 @@
+def solution(k, ranges):
+    lst = [k] 
+    def calculate(start, end, n ) :
+        total = 0
+        if start == n + end :
+            return 0
+        if start > n + end :
+            return -1
+        for i in range(start, n + end) :
+            total += (lst[i] + lst[i+1])/2
+        return total
+    
+    while k != 1 :
+        if k % 2 == 0 :
+            k //= 2
+        else :
+            k = k* 3 + 1
+        lst.append(k)
+    dp = []
+    answer = []
+    for r in ranges :
+        start, end = r
+        answer.append(calculate(start, end ,len(lst)-1))
+    return answer
