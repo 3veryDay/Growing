a8bf61ae0b90c60b94b1da28a6c13ce32124b41d
지현서 <113419425+3veryDay@users.noreply.github.com>
2025-03-09 21:10:40 +0900

    unsolved(N-Queen)

--- ProgrammersLv2/N-Queen.py
+++ ProgrammersLv2/N-Queen.py
@@ -20,3 +20,28 @@ def solution(n):
         if status == 0 :
             answer += 1
     return answer
+'''
+백트래킹으로 했어야 함
+내일은 백 트래킹 연습하자/.
+'''
+
+def getAns(n, y, garo, left, right):
+    ans = 0
+    #y는 순차적으로 늘리는 애
+    
+    # 모든 행에 대해서 다 구했을 때
+    if y == n :
+        ans += 1
+    else :
+        for i in range(n) :
+            if garo[i] or left[i+y] or right[i-y+n] :
+                continue
+            garo[i]  =left[i+y] = right[i-y+n]=True
+            ans += getAns(n, y+1, garo, left, right)
+            garo[i] = left[i+y] = right[i-y +n] = False
+    return ans
+
+def solution(n) :
+    ans = getAns(n, 0, [False]*n , [False]*(2*n), [False]*(2*n))
+    return ans
+    
