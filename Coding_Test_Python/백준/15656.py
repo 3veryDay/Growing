25ff47bd6c37af7a7a272ce8d242a84532d10285
지현서 <113419425+3veryDay@users.noreply.github.com>
2025-07-17 20:44:12 +0900

    Create 15656.py

---
+++ Coding_Test_Python/백준/15656.py
@@ -0,0 +1,22 @@
+N, M = map(int, input().split())
+
+numbers = list(map(int, input().split()))
+numbers. sort()
+
+
+def backtrack(path, depth) :
+    # 종료 조건
+    if depth == M :
+        print(*path)
+        return
+    
+    # 후보군 탐색
+    for next_idx in range(N) :
+        # 상태 변경
+        path.append(numbers[next_idx])
+        backtrack(path, depth + 1)
+        # 상태 복원
+        path.pop()
+
+for first_idx in range(N) :
+    backtrack([numbers[first_idx]], 1)
