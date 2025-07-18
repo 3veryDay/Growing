0914d8d7d25eb93945fc0b500adc1c9e1886b96c
지현서 <113419425+3veryDay@users.noreply.github.com>
2025-07-18 13:52:34 +0900

    Update daily_issue.py

--- daily_issue.py
+++ daily_issue.py
@@ -1,32 +1,18 @@
-import requests
+import json
 import random
 import os
+import requests
 
 GITHUB_REPO = "3veryday/Growing"
 GITHUB_TOKEN = os.environ["IMPLEMENTATION_GH_TOKEN"]
 
-# level 9~10 (실버2,1), level 15~16 (골드5,4) 따로
-API_URLS = [
-    "https://solved.ac/api/v3/search/problem?query=tag:구현%20level:9..10&sort=random",
-    "https://solved.ac/api/v3/search/problem?query=tag:구현%20level:15..16&sort=random"
-]
-
 def get_random_problem():
-    problems = []
-
-    for url in API_URLS:
-        res = requests.get(url)
-        data = res.json()
-        items = data.get("items", [])
-        problems.extend(items)
-
-    if not problems:
-        raise ValueError("문제 리스트가 비어 있습니다.")
-
-    problem = random.choice(problems)
+    with open("problem_list.json", "r", encoding="utf-8") as f:
+        problems = json.load(f)
+    selected = random.choice(problems)
     return {
-        "title": f"{problem['problemId']} - {problem['titleKo']}",
-        "url": f"https://www.acmicpc.net/problem/{problem['problemId']}"
+        "title": f"{selected['id']} - {selected['title']}",
+        "url": f"https://www.acmicpc.net/problem/{selected['id']}"
     }
 
 def create_github_issue(title, url):
